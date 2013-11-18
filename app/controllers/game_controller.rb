4e4a3cb4d172fc1b50c5a9dfeba49d814eb348b4
ivannasya <vagrant@vagrant.(none)>
2013-11-18 18:19:20 +0000

    add time

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -7,20 +7,28 @@ class GameController < ApplicationController
 	end
 
   	def create
+  		# Statistics of visits 
   		case params[:step]
 		when "1" then session[:house] += 1
   		when "2" then session[:work] += 1
   		when "3" then session[:harats] += 1
 	    end
 
+	    # Time of day
+	    time = ["", "Утро", "День", "Вечер", "Ночь"]
+  		@time = time[session[:way].size]
+
+  		# Аdd the selected location in the way
   		params[:step] = "1" if params[:step].nil?
   		p session[:way] << params[:step]
   		
-  		# time = ["", "Утро", "День", "Вечер", "Ночь"]
-  		# p @time = time[session[:way].size]
-
-  		@image  = Step.location(session[:way]).image
-  		@guide  = Step.location(session[:way]).guide
-  		@button = Step.location(session[:way]).button
+  		# Create new location
+		# begin
+	  		@image  = Step.location(session[:way]).image
+	  		@guide  = Step.location(session[:way]).guide
+	  		@button = Step.location(session[:way]).button
+    	#	rescue
+		# 		redirect_to new_game_url
+		# end
   	end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,6 @@ class SessionsController < ApplicationController
 
   	def create
       session[:way] = []
-
       unless session[:user]
         	session[:user] = Digest::MD5.hexdigest((0...5).map{('a'..'z').to_a[rand(26)]}.join)
           session[:house] = 0
