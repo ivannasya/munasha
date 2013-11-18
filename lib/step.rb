37370cebd1f5c5c8c52407625e0592a067a6638c
ivannasya <vagrant@vagrant.(none)>
2013-11-18 22:23:51 +0000

    all harat's

--- lib/step.rb
+++ lib/step.rb
@@ -10,7 +10,7 @@ class Step
 				 	[{id: "0", guide: "guide_nastya_1.png", closing_event: "game_url", text: ["- Время просыпаться!", "- Время принимать решения...", "- На что падет твой выбор сегодня?", "- И да... ужасно выглядишь!", "=)"]}, 
 					 {id: "1", guide: "guide_nastya_1.png", closing_event: "game_url", text: ["- Тяжелая ночь, да?", "- Ну, что ж... Я здесь не для того, чтобы осуждать тебя...", "- Спи дальше! Пока жизнь проходит мимо. Дни, недели, месяцы, годы!", "- Все это не важно, когда ХОЧЕТСЯ ПОСПАТЬ!-Правда?" ]},
 					 {id: "2", guide: "guide_nastya_1.png", closing_event: "game_url", text: ["- Work Hard! Этот выбор стоит аплодисментов.", "*хлоп*...*хлоп*", "- И вот ты медленно сползаешь с кровати, почти неосознанно идешь умываться.", "- Бодрящий душ... не помогает.", "- По прежнему на автопилоте ты идешь одеваться.", "- Вызываешь такси, завариваешь кофе... И, кажется, вот он - момент прозрения...", "- Но... На самом деле, ты все еще в кровати, а все выше описанное - плод твоего воспаленного, но ответственного воображения.", "- Может хватит? Пора вставать.", "=)"]},
-				  	 {id: "3", guide: "guide_nastya_1.png", closing_event: "game_url", text: ["- Ты серьезно?", "- ...", "- ...", "- ...", "- Я...", "Я даже не знаю, что на это сказать...", "- ...", "- А меня возьмешь?","=)"]}
+				  	 {id: "3", guide: "guide_nastya_1.png", closing_event: "game_url", text: ["- Ты серьезно?", "- ...", "- ...", "- ...", "- Я...", "- Я даже не знаю, что на это сказать...", "- ...", "- А меня возьмешь?","=)"]}
 					],
 				  	[{title: "Спать дальше", id: "1"}, {title: "На работу", id: "2"}, {title: "В Harat's", id: "3"}]
 				)
@@ -32,10 +32,10 @@ class Step
 					],
 				  	[{title: "Спать дальше", id: "1"}]
 				)
-		# house-house-house
+		# house-house-house: Sweet Dreams
 		when ["1", "1", "1", "1"]
 			Location.new("home.png",
-				 	[{id: "0", guide: "guide_nastya_1.png", closing_event: "game_url", text: ["- SWEET DREAMS!","- Для продолжения нажми пробел."]}],
+				 	[{id: "0", guide: "guide_nastya_1.png", closing_event: "new_game_url", text: ["- SWEET DREAMS!","- Для продолжения нажми пробел."]}],
 				  	[]
 				)
 		#  work
@@ -83,21 +83,67 @@ class Step
  		# work-harat's-house-rayon: Rayon
 		when ["1", "2", "3", "1", "4"] 
 				Location.new("rayon.png",
-				 	[{id: "0", guide: "guide_nastya_1.png", closing_event: "new_game_url", text: ["-тут должен быть текст", "- Для продолжения нажми пробел."]}],
+				 	[{id: "0", guide: "guide_nastya_1.png", closing_event: "new_game_url", text: ["- Для продолжения нажми пробел."]}],
 				 	[]
 				)
 		# work-harat's-house-secret_final: Secret Final
 		when ["1", "2", "3", "1", "5"] 
 				Location.new("secret_final.png",
-				 	[{id: "0", guide: "guide_nastya_1.png", closing_event: "new_game_url", text: ["Молодец. Открыл секретную концовку. Теперь сначала.", "- Для продолжения нажми пробел."]}],
+				 	[{id: "0", guide: "guide_nastya_1.png", closing_event: "new_game_url", text: ["- Молодец. Открыл секретную концовку. Теперь сначала.", "- Для продолжения нажми пробел."]}],
 				 	[]
 				)
 		# harat's
   		when ["1", "3"]
   			Location.new("harats.png",
-				 [{id: "0", guide: "guide_nick_1.png", closing_event: "game_url", text: []}
-				 ],
-				 [{title: "Поехать Домой", id: "1"}])
+				 	[{id: "0", guide: "guide_slava_1.png", closing_event: "game_url", text: ["- Ай-на-нэ-на-нэ! Рад тэбя видеть здэс в такое время!","- Ты когда-нибудь дрэссировал мэдведя?","- О-о-о-о-о, ты жизни нэ знаешь, если нэ дрэссировал!","- Слушай… Мэжду нами… Есть роллэкс, недорого. Возьмешь? Тэбе как другу скидка!","- Ты же останэшься здэсь подольше?"]}, 
+					 {id: "1", guide: "guide_slava_2.png", closing_event: "game_url", text: ["- Воу, воу, воу! Ну зачэм, зачэм ты так?","- Здэсь хорошая компания, теплая атмосфера! Куда тэбе спешить?"]},
+					 {id: "2", guide: "guide_slava_2.png", closing_event: "game_url", text: ["- Друуууг, мы же оба знаем, что ты нэ этого хочешь!","- Нэмного цыганской магии, вуаля!"]},
+				  	 {id: "3", guide: "guide_slava_1.png", closing_event: "game_url", text: ["- Мать моих мэдведей! Да сэгодня просто праздник какой-то!","- Идем обниму тэбя! Гуляем за мой счет.","- Кстати, тут такое дэло… Одолжи нэмного, брат!"]}
+					],
+				  	[{title: "Поехать домой", id: "1"}, {title: "Поехать в офис", id: "2"}, {title: "Остаться в Harat’s", id: "3"}]
+				)
+		# harat's-harat's
+  		when ["1", "3", "1"]
+				Location.new("harats.png",
+				 	[{id: "0", guide: "guide_slava_1.png", closing_event: "game_url", text: ["- *убирает зажигалку, бумажник, ключи во внутренний карман, говорит под нос*","- О-о-о, друг, да ты пэребрал, Ничего, я друга в бэде не брошу!","- *тащит обмякшее тело к выходу, напевает*","- Щас тэбя я отнесу…","- Возле паба положу…","- Может, кто и подбэрет","- Ну, а я обратно в паб."]},
+				 	 {id: "1", guide: "empty_guide.png", closing_event: "game_url", text: ["- …","- …","- *сверчки*","- …"]}
+					],
+				 	[{title: "Поехать Домой", id: "1"}]
+				)
+		# harat's-harat's
+  		when ["1", "3", "2"]
+				Location.new("harats.png",
+				 	[{id: "0", guide: "guide_slava_1.png", closing_event: "game_url", text: ["- *убирает зажигалку, бумажник, ключи во внутренний карман, говорит под нос*","- О-о-о, друг, да ты пэребрал, Ничего, я друга в бэде не брошу!","- *тащит обмякшее тело к выходу, напевает*","- Щас тэбя я отнесу…","- Возле паба положу…","- Может, кто и подбэрет","- Ну, а я обратно в паб."]},
+				 	 {id: "1", guide: "empty_guide.png", closing_event: "game_url", text: ["- …","- …","- *сверчки*","- …"]}
+					],
+				 	[{title: "Поехать Домой", id: "1"}]
+				)
+		# harat's-harat's
+  		when ["1", "3", "3"]
+				Location.new("harats.png",
+				 	[{id: "0", guide: "guide_slava_1.png", closing_event: "game_url", text: ["- *убирает зажигалку, бумажник, ключи во внутренний карман, говорит под нос*","- О-о-о, друг, да ты пэребрал, Ничего, я друга в бэде не брошу!","- *тащит обмякшее тело к выходу, напевает*","- Щас тэбя я отнесу…","- Возле паба положу…","- Может, кто и подбэрет","- Ну, а я обратно в паб."]},
+				 	 {id: "1", guide: "empty_guide.png", closing_event: "game_url", text: ["- …","- …","- *сверчки*","- …"]}
+					],
+				 	[{title: "Поехать Домой", id: "1"}]
+				)
+		# harat's-harat's-harat's: All Road Lead To Harat’s
+		when ["1", "3", "1", "1"]
+			Location.new("home.png",
+				 	[{id: "0", guide: "guide_nastya_1.png", closing_event: "new_game_url", text: ["- All Road Lead To Harat’s!","- Для продолжения нажми пробел."]}],
+				  	[]
+				)
+		# harat's-harat's-harat's: All Road Lead To Harat’s
+		when ["1", "3", "2", "1"]
+			Location.new("home.png",
+				 	[{id: "0", guide: "guide_nastya_1.png", closing_event: "new_game_url", text: ["- All Road Lead To Harat’s!","- Для продолжения нажми пробел."]}],
+				  	[]
+				)
+		# harat's-harat's-harat's: All Road Lead To Harat’s
+		when ["1", "3", "3", "1"]
+			Location.new("home.png",
+				 	[{id: "0", guide: "guide_nastya_1.png", closing_event: "new_game_url", text: ["- All Road Lead To Harat’s!","- Для продолжения нажми пробел."]}],
+				  	[]
+				)		
 		else
 		end
 	end
