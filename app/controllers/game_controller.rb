07a7f59c63cc081540aa42f23453ae14fd3929de
ivannasya <vagrant@vagrant.(none)>
2013-11-19 00:53:52 +0000

    badge

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -22,28 +22,42 @@ class GameController < ApplicationController
 	    end
 
 	    # Statistics of wins
+	    @badge = false
 	    case session[:way]
-		when ["1", "1", "1", "1"] 		then session[:win] << 1
-		when ["1", "2", "2", "1"] 		then session[:win] << 2
-		when ["1", "2", "3", "1", "4"]  then session[:win] << 3
-		when ["1", "2", "3", "1", "5"]  then session[:win] << 4
-		when ["1", "3", "1", "1"] 		then session[:win] << 5
-		when ["1", "3", "2", "1"] 		then session[:win] << 5
-		when ["1", "3", "3", "1"] 		then session[:win] << 5
+		when ["1", "1", "1", "1"]
+			session[:win] << 1
+			@badge = true
+		when ["1", "2", "2", "1"]
+		 	session[:win] << 2
+		 	@badge = true
+		when ["1", "2", "3", "1", "4"]
+			session[:win] << 3
+			@badge = true
+		when ["1", "2", "3", "1", "5"]
+			session[:win] << 4
+			@badge = true
+		when ["1", "3", "1", "1"]
+			session[:win] << 5
+			@badge = true
+		when ["1", "3", "2", "1"]
+		    session[:win] << 5
+		    @badge = true
+		when ["1", "3", "3", "1"]
+			session[:win] << 5
+			@badge = true
 	    end
 	    @wins = session[:win].uniq
-
 	    # Time of day
 	    time = ["", "Утро", "День", "Вечер", "Ночь", "Ночь"]
   		@time = time[session[:way].size]
   		
   		# Create new location
-		# begin
+		begin
 	  		@image  = Step.location(session[:way]).image
 	  		@guide  = Step.location(session[:way]).guide
 	  		@button = Step.location(session[:way]).button
-  #   		rescue
-		# 		redirect_to new_game_url
-		# end
+    	rescue
+			redirect_to new_game_url
+		end
   	end
 end
